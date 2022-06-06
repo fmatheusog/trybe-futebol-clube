630c8d7994a9b902ee95b8974e36c42d39b0b003
Felipe Matheus <fmatheusog@gmail.com>
2022-06-06 09:11:35 -0300

    feat: creates auth service's login method

---
+++ app/backend/src/services/AuthenticationService.ts
@@ -0,0 +1,33 @@
+import { compare } from 'bcryptjs';
+import User from '../database/models/users';
+import TokenService from './TokenService';
+
+export default class AuthenticationService {
+  private user: User | null;
+  private incorrectDataErrorMessage = 'Incorrect email or password';
+  private tokenService = new TokenService();
+
+  async login(email: string, password: string): Promise<object | null> {
+    this.user = await User.findOne({
+      where: { email },
+    });
+
+    if (!this.user) throw new Error(this.incorrectDataErrorMessage);
+
+    const isPasswordCorrect = await compare(password, this.user.password);
+    if (!isPasswordCorrect) throw new Error(this.incorrectDataErrorMessage);
+
+    const { id } = this.user;
+    const token = this.tokenService.generate(id);
+
+    return {
+      user: {
+        id,
+        username: this.user.username,
+        role: this.user.role,
+        email: this.user.email,
+      },
+      token,
+    };
+  }
+}
