b3315b1bbc655604d55ec4e507f0cde0d7257d34
Felipe Matheus <fmatheusog@gmail.com>
2022-06-06 14:02:28 -0300

    fix: corrects jwt secret file path

--- app/backend/src/services/TokenService.ts
+++ app/backend/src/services/TokenService.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 import { Secret, sign, SignOptions, verify } from 'jsonwebtoken';
 
 export default class TokenService {
-  private secret: Secret = readFileSync('../jwt.evaluation.key', { encoding: 'utf-8' });
+  private secret: Secret = readFileSync('./jwt.evaluation.key', { encoding: 'utf-8' });
   private config: SignOptions = {
     expiresIn: '15m',
     algorithm: 'HS256',
