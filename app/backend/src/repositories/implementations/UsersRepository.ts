7842a5edd29e4344acf515319b62f3ab59458866
Felipe Matheus <fmatheusog@gmail.com>
2022-06-13 15:36:34 -0300

    refact: identation correction

--- app/backend/src/repositories/implementations/UsersRepository.ts
+++ app/backend/src/repositories/implementations/UsersRepository.ts
@@ -3,6 +3,7 @@ import IUsersRepository from '../IUsersRepository';
 
 export default class UsersRepository implements IUsersRepository {
   private users = User;
+
   async findByEmail(email: string): Promise<User> {
     const user = await this.users.findOne({ where: { email } });
 
